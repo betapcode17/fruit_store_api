ac47f1745267186235fc424c8b256fb4843dfd74
Trần Quốc Đạt <164189973+betapcode17@users.noreply.github.com>
2025-08-27 10:07:23 +0700

    Delete .history directory

--- .history/app/models_20250826202055.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column,Integer,String,Float
-from .database import Base
-
-class Fruit(Base):
-    __tablename__ = "fruits"
-
-    
--- .history/main_20250826204529.py
+++
@@ -1,7 +0,0 @@
-# init_db.py
-from database import Base, engine
-from models import User, Fruit, Bill, BillDetail
-
-print("Creating tables in database...")
-Base.metadata.create_all(bind=engine)
-print(" Done!")
